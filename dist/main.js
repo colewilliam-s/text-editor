2baf239a59af8ab5c264f90aee6a2912fd7c515d
colewilliam-s <colewilliams0614@gmail.com>
2025-10-21 18:37:50 -0400

    so many file changes because of the fact that downloaded the framework to create my own desktop app. Whenever I run npm start in the terminal now (in this directory), I now run a full fledged desktop app tahat I can customize using web development techniques. This will be really easy creating a text editor for this.

---
+++ dist/main.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// src/main.ts
+const electron_1 = require("electron");
+let mainWindow;
+function createWindow() {
+    mainWindow = new electron_1.BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+    mainWindow.loadFile('html/index.html');
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+electron_1.app.whenReady().then(createWindow);
+electron_1.app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
+});
+electron_1.app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+    // src/main.ts
+    import { app, BrowserWindow } from 'electron';
+
+    let mainWindow: BrowserWindow | null;
+
+    function createWindow() {
+      mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false,
+        },
+      });
+
+      mainWindow.loadFile('html/index.html');
+
+      mainWindow.on('closed', () => {
+        mainWindow = null;
+      });
+    }
+
+    app.whenReady().then(createWindow);
+
+    app.on('window-all-closed', () => {
+      if (process.platform !== 'darwin') {
+        app.quit();
+      }
+    });
+
+    app.on('activate', () => {
+      if (mainWindow === null) {
+        createWindow();
+      }
+    });
